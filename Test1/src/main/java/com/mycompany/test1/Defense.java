241d2e33deded53b7d923cfe0948285d3cbec448
Krysmo <holmieres@gmail.com>
2017-04-18 16:24:25 +0200

    Def2
    Modification de défense

--- Test1/src/main/java/com/mycompany/test1/Defense.java
+++ Test1/src/main/java/com/mycompany/test1/Defense.java
@@ -1,6 +1,13 @@
 package com.mycompany.test1;
 
+import cz.cuni.amis.pogamut.base3d.worldview.object.ILocated;
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
+import cz.cuni.amis.pogamut.base3d.worldview.object.Rotation;
+import cz.cuni.amis.pogamut.unreal.communication.messages.UnrealId;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbcommands.Rotate;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.NavPoint;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Player;
+import java.util.logging.Level;
 
 
 
@@ -11,9 +18,45 @@ public class Defense extends Behavior{
     }
     
     @Override
-    public void performed(){
-            getBot().getAct().act(new Rotate().setAmount(32000));
-           
+    public void performed(){      
+        
+           /*
+        Location ennemiLocation = getBot().
+            getBot().getLog().info("Gauche" + ennemiLocation);
+        Location recul = getBot().getNavigation().getNearestNavPoint(getBot().getBot()).getLocation();
+            getBot().getLog().info("Gauche" + recul);
+        Location uneLocation = ennemiLocation.sub(recul);
+            getBot().getLog().info("Gauche" + uneLocation);
+        */
+        getBot().getNavigation().stopNavigation();
+        getBot().getMove().turnVertical(180);
+        
+        getBot().getMove().doubleJump();
+        //Location uneLocation = getBot().getInfo().getLocation();
+        //getBot().getLog().log(Level.INFO, "Gauche{0} ", uneLocation);
+        if(!getBot().getPlayers().canSeeEnemies())
+            getBot().getMove().turnVertical(180);
+            
+        //UnrealId locationEnnemi = getBot().getSenses().getNoiseSource();
+            //getBot().getLog().info("Gauche" + locationEnnemi);
+              
     }
+    /*
+    private void dodge() {
+        int uneValeurRandom = getBot().getRandom().nextInt(5);
+        if(uneValeurRandom < 5){
+            Player player1 = getBot().getInfo().getNearestPlayer();
+            NavPoint recul = getBot().getNavigation().getNearestNavPoint(getBot().getBot());
+            Location uneLocation = player1.getLocation().sub(recul.getLocation());
 
+            getBot().getMove().doubleJump();
+            getBot().getLog().info("Gauche");
+        }
+        if(uneValeurRandom >= 5){
+            getBot().getMove().doubleJump();
+            getBot().getLog().info("Jump");
+        }
+                
+    }
+*/
 }
