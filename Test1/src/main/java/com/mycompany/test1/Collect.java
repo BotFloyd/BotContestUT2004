c64d2251d991560644c5144101f33b55ed5f7e2c
kedrisse <kedrisse@hotmail.fr>
2017-04-18 17:33:10 +0200

    Priorité des groupes d'objets

--- Test1/src/main/java/com/mycompany/test1/Collect.java
+++ Test1/src/main/java/com/mycompany/test1/Collect.java
@@ -25,6 +25,7 @@ import cz.cuni.amis.pogamut.ut2004.agent.navigation.IUT2004Navigation;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.ItemType;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.UT2004ItemType;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Item;
+import java.util.Map;
 
 /**
  *
@@ -37,7 +38,8 @@ public class Collect extends Behavior{
     IUT2004Navigation navigation;
     Weaponry weaponry;
     NavPoints nav;
-      
+    Map<UT2004ItemType, Double> groupPriority; 
+    
     public Collect(EmptyBot unBot) {
         super(unBot);
     }
@@ -55,6 +57,7 @@ public class Collect extends Behavior{
         initVars();
         if(! navigation.isNavigating()){ 
             Item selectedItem = null;
+            updateGroupPriority();
             if(! info.isHealthy() && selectedItem == null){
                 selectedItem = items.getPathNearestSpawnedItem(ItemType.Category.HEALTH);
             }
@@ -127,5 +130,80 @@ public class Collect extends Behavior{
                 navigation.navigate(selectedItem);
             }
         }
+    }
+    
+    private void updateGroupPriority(){
+        
+        groupPriority.put(UT2004ItemType.MINI_HEALTH_PACK, 3.0*(199-info.getHealth())/199);
+        groupPriority.put(UT2004ItemType.ADRENALINE_PACK, 1.0*(99-info.getAdrenaline())/99);
+        groupPriority.put(UT2004ItemType.SHIELD_PACK, 4.0*(50-info.getLowArmor())/50);
+        groupPriority.put(UT2004ItemType.SUPER_SHIELD_PACK, 6.0*(100-info.getHighArmor())/100);
+        
+        if(weaponry.hasWeapon(UT2004ItemType.ROCKET_LAUNCHER)){
+            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER, 0.0);
+            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 10.0*(weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER, 10.0);
+            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 5.0*(weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.SNIPER_RIFLE)){
+            groupPriority.put(UT2004ItemType.SNIPER_RIFLE, 0.0);
+            groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 9.0*(weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.SNIPER_RIFLE, 9.0);
+            groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 4.5*(weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.FLAK_CANNON)){
+            groupPriority.put(UT2004ItemType.FLAK_CANNON, 0.0);
+            groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 8.0*(weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.FLAK_CANNON_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.FLAK_CANNON, 8.0);
+            groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 4.0*(weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.FLAK_CANNON_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.MINIGUN)){
+            groupPriority.put(UT2004ItemType.MINIGUN, 0.0);
+            groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 7.0*(weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.MINIGUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.MINIGUN, 7.0);
+            groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 3.5*(weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.MINIGUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO));
+        }
+       
+        if(weaponry.hasWeapon(UT2004ItemType.LIGHTNING_GUN)){
+            groupPriority.put(UT2004ItemType.LIGHTNING_GUN, 0.0);
+            groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 6.0*(weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.LIGHTNING_GUN, 6.0);
+            groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 3.0*(weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.SHOCK_RIFLE)){
+            groupPriority.put(UT2004ItemType.SHOCK_RIFLE, 0.0);
+            groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 5.0*(weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.SHOCK_RIFLE, 5.0);
+            groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 2.5*(weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.LINK_GUN)){
+            groupPriority.put(UT2004ItemType.LINK_GUN, 0.0);
+            groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 4.0*(weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.LINK_GUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.LINK_GUN, 4.0);
+            groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 2.0*(weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.LINK_GUN_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO));
+        }
+        
+        if(weaponry.hasWeapon(UT2004ItemType.BIO_RIFLE)){
+            groupPriority.put(UT2004ItemType.BIO_RIFLE, 0.0);
+            groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 3.0*(weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.BIO_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO));
+        } else {
+            groupPriority.put(UT2004ItemType.BIO_RIFLE, 3.0);
+            groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 1.5*(weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.BIO_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO));
+        }
+        
+        groupPriority.put(UT2004ItemType.ASSAULT_RIFLE_AMMO, 1.0*(weaponry.getMaxAmmo(UT2004ItemType.ASSAULT_RIFLE_AMMO)-weaponry.getWeaponAmmo(UT2004ItemType.ASSAULT_RIFLE_AMMO))/weaponry.getMaxAmmo(UT2004ItemType.ASSAULT_RIFLE_AMMO));
+        
     }
 }
