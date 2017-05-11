02c254defeac7728626675eb43fc7a96b10ae813
Krysmo <holmieres@gmail.com>
2017-05-11 12:00:18 +0200

    Modif priorité collect

--- Repliquant/src/main/java/state/concrete/Collect.java
+++ Repliquant/src/main/java/state/concrete/Collect.java
@@ -41,11 +41,12 @@ public class Collect extends Behavior {
             Item selectedItem = null;
             Double highestPriority = 0.0;
             Double priority;
-            updateGroupPriority();
-            if (!items.getSpawnedItems().values().isEmpty()) {
+            updateGroupPriority(unBot);
+            unBot.getLog().severe("Groupe priority : " + groupPriority.toString());
+            if (!items.getSpawnedItems().values().isEmpty() && !navigation.isNavigating()) {
                 for (Item item : tabooItems.filter(items.getSpawnedItems().values())) {
                     if (groupPriority.containsKey(item.getType()) && (groupPriority.get(item.getType()) > 0)) {
-                        priority = groupPriority.get(item.getType()) * 200 - unBot.getFwMap().getDistance(info.getNearestNavPoint(), item.getNavPoint());
+                        priority = groupPriority.get(item.getType()) * 200 - unBot.getBot().getLocation().getDistance(item.getLocation());
                         if (priority == highestPriority) {
                             if (selectedItem == null || (groupPriority.get(item.getType()) > groupPriority.get((UT2004ItemType) selectedItem.getType()))) {
                                 selectedItem = item;
@@ -57,30 +58,43 @@ public class Collect extends Behavior {
                     }
                 }
             }
-
-            if (selectedItem == null) {
-                navigation.navigate(nav.getRandomNavPoint());
-            } else {
-                navigation.navigate(selectedItem);
-                tabooItems.add(selectedItem, (unBot.getRandom().nextDouble() * 0.5 + 1) * items.getItemRespawnTime(selectedItem));
+                
+            if(!navigation.isNavigating()){
+                if (selectedItem == null) {
+                    navigation.navigate(nav.getRandomNavPoint());
+                } else {
+                    unBot.getLog().severe(" Selected Item : " + selectedItem.getType());
+                    navigation.navigate(selectedItem);
+                    tabooItems.add(selectedItem, (unBot.getRandom().nextDouble() * 0.5 + 1) * items.getItemRespawnTime(selectedItem));
+                }
             }
         }
     }
 
-    private void updateGroupPriority() {
-
+    private void updateGroupPriority(Repliquant unBot) {
+        groupPriority.clear();
         if (risque >= 0) {
-            groupPriority.put(UT2004ItemType.MINI_HEALTH_PACK, 2.0 * (199 - info.getHealth()) / 199.0);
-            groupPriority.put(UT2004ItemType.HEALTH_PACK, 8.0 * (100 - info.getHealth()) / 100.0);
-            groupPriority.put(UT2004ItemType.SUPER_HEALTH_PACK, 8.0 * (199 - info.getHealth()) / 199.0);
-            groupPriority.put(UT2004ItemType.SHIELD_PACK, 5.0 * (50 - info.getLowArmor()) / 50.0);
-            groupPriority.put(UT2004ItemType.SUPER_SHIELD_PACK, 8.0 * (100 - info.getHighArmor()) / 100.0);
+            if(unBot.getInfo().getHealth() < 150)
+                groupPriority.put(UT2004ItemType.MINI_HEALTH_PACK, 2.0 * (199 - info.getHealth()) / 199.0);
+            if(unBot.getInfo().getHealth() < 100)
+                groupPriority.put(UT2004ItemType.HEALTH_PACK, 8.0 * (100 - info.getHealth()) / 100.0);
+            if(unBot.getInfo().getHealth() < 199)
+                groupPriority.put(UT2004ItemType.SUPER_HEALTH_PACK, 8.0 * (199 - info.getHealth()) / 199.0);
+            if(unBot.getInfo().getArmor() < 50)
+                groupPriority.put(UT2004ItemType.SHIELD_PACK, 5.0 * (50 - info.getLowArmor()) / 50.0);
+            if(unBot.getInfo().getArmor() < 100)
+                groupPriority.put(UT2004ItemType.SUPER_SHIELD_PACK, 8.0 * (100 - info.getHighArmor()) / 100.0);
         } else {
-            groupPriority.put(UT2004ItemType.MINI_HEALTH_PACK, 4.0 * (199 - info.getHealth()) / 199.0);
-            groupPriority.put(UT2004ItemType.HEALTH_PACK, 16.0 * (100 - info.getHealth()) / 100.0);
-            groupPriority.put(UT2004ItemType.SUPER_HEALTH_PACK, 16.0 * (199 - info.getHealth()) / 199.0);
-            groupPriority.put(UT2004ItemType.SHIELD_PACK, 10.0 * (50 - info.getLowArmor()) / 50.0);
-            groupPriority.put(UT2004ItemType.SUPER_SHIELD_PACK, 16.0 * (100 - info.getHighArmor()) / 100.0);
+            if(unBot.getInfo().getHealth() < 150)
+                groupPriority.put(UT2004ItemType.MINI_HEALTH_PACK, 4.0 * (199 - info.getHealth()) / 199.0);
+            if(unBot.getInfo().getHealth() < 100)
+                groupPriority.put(UT2004ItemType.HEALTH_PACK, 16.0 * (100 - info.getHealth()) / 100.0);
+            if(unBot.getInfo().getHealth() < 199)
+                groupPriority.put(UT2004ItemType.SUPER_HEALTH_PACK, 16.0 * (199 - info.getHealth()) / 199.0);
+            if(unBot.getInfo().getArmor() < 50)
+                groupPriority.put(UT2004ItemType.SHIELD_PACK, 10.0 * (50 - info.getLowArmor()) / 50.0);
+            if(unBot.getInfo().getArmor() < 100)
+                groupPriority.put(UT2004ItemType.SUPER_SHIELD_PACK, 16.0 * (100 - info.getHighArmor()) / 100.0);
         }
         
         if (weaponry.hasLoadedWeapon()) {
@@ -96,70 +110,96 @@ public class Collect extends Behavior {
         groupPriority.put(UT2004ItemType.REDEEMER, 999.0);
         groupPriority.put(UT2004ItemType.ION_PAINTER, 999.0);
 
-        if (weaponry.hasWeapon(UT2004ItemType.ROCKET_LAUNCHER)) {
+        if (weaponry.hasWeapon(UT2004ItemType.ROCKET_LAUNCHER)) {                        
             groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER, 0.0);
-            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 10.0 * (weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.ROCKET_LAUNCHER)) / weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.ROCKET_LAUNCHER) != weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER))
+                groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 10.0 * (weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.ROCKET_LAUNCHER)) / weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER, 10.0);
-            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 5.0 * (weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.ROCKET_LAUNCHER)) / weaponry.getMaxAmmo(UT2004ItemType.ROCKET_LAUNCHER_AMMO));
+            groupPriority.put(UT2004ItemType.ROCKET_LAUNCHER_AMMO, 0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.SNIPER_RIFLE)) {
             groupPriority.put(UT2004ItemType.SNIPER_RIFLE, 0.0);
-            groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 9.0 * (weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SNIPER_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.SNIPER_RIFLE) != weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE))
+                groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 9.0 * (weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SNIPER_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.SNIPER_RIFLE, 9.0);
-            groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 4.5 * (weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SNIPER_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SNIPER_RIFLE_AMMO));
+            groupPriority.put(UT2004ItemType.SNIPER_RIFLE_AMMO, 0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.FLAK_CANNON)) {
             groupPriority.put(UT2004ItemType.FLAK_CANNON, 0.0);
-            groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 8.0 * (weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.FLAK_CANNON)) / weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.FLAK_CANNON) != weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON))
+                groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 8.0 * (weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.FLAK_CANNON)) / weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.FLAK_CANNON, 8.0);
-            groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 4.0 * (weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.FLAK_CANNON)) / weaponry.getMaxAmmo(UT2004ItemType.FLAK_CANNON_AMMO));
+            groupPriority.put(UT2004ItemType.FLAK_CANNON_AMMO, 0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.MINIGUN)) {
             groupPriority.put(UT2004ItemType.MINIGUN, 0.0);
-            groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 7.0 * (weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.MINIGUN)) / weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.MINIGUN) != weaponry.getMaxAmmo(UT2004ItemType.MINIGUN))
+                groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 7.0 * (weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.MINIGUN)) / weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.MINIGUN, 7.0);
-            groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 3.5 * (weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.MINIGUN)) / weaponry.getMaxAmmo(UT2004ItemType.MINIGUN_AMMO));
+            groupPriority.put(UT2004ItemType.MINIGUN_AMMO, 0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.LIGHTNING_GUN)) {
             groupPriority.put(UT2004ItemType.LIGHTNING_GUN, 0.0);
-            groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 6.0 * (weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LIGHTNING_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.LIGHTNING_GUN) != weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN))
+                groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 6.0 * (weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LIGHTNING_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.LIGHTNING_GUN, 6.0);
-            groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO, 3.0 * (weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LIGHTNING_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LIGHTNING_GUN_AMMO));
+            groupPriority.put(UT2004ItemType.LIGHTNING_GUN_AMMO,0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.SHOCK_RIFLE)) {
             groupPriority.put(UT2004ItemType.SHOCK_RIFLE, 0.0);
-            groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 5.0 * (weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SHOCK_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.SHOCK_RIFLE) != weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE))
+                groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 5.0 * (weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SHOCK_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.SHOCK_RIFLE, 5.0);
-            groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO, 2.5 * (weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.SHOCK_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.SHOCK_RIFLE_AMMO));
+            groupPriority.put(UT2004ItemType.SHOCK_RIFLE_AMMO,0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.LINK_GUN)) {
             groupPriority.put(UT2004ItemType.LINK_GUN, 0.0);
-            groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 4.0 * (weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LINK_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.LINK_GUN) != weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN))
+                groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 4.0 * (weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LINK_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.LINK_GUN, 4.0);
-            groupPriority.put(UT2004ItemType.LINK_GUN_AMMO, 2.0 * (weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.LINK_GUN)) / weaponry.getMaxAmmo(UT2004ItemType.LINK_GUN_AMMO));
+            groupPriority.put(UT2004ItemType.LINK_GUN_AMMO,0.0);
         }
 
         if (weaponry.hasWeapon(UT2004ItemType.BIO_RIFLE)) {
             groupPriority.put(UT2004ItemType.BIO_RIFLE, 0.0);
-            groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 3.0 * (weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.BIO_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO));
+            if(weaponry.getAmmo(UT2004ItemType.BIO_RIFLE) != weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE))
+                groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 3.0 * (weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.BIO_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO));
+            else 
+                groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 0.0);
         } else {
             groupPriority.put(UT2004ItemType.BIO_RIFLE, 3.0);
-            groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 1.5 * (weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.BIO_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.BIO_RIFLE_AMMO));
+            groupPriority.put(UT2004ItemType.BIO_RIFLE_AMMO, 0.0);
         }
 
         groupPriority.put(UT2004ItemType.ASSAULT_RIFLE_AMMO, 1.0 * (weaponry.getMaxAmmo(UT2004ItemType.ASSAULT_RIFLE_AMMO) - weaponry.getWeaponAmmo(UT2004ItemType.ASSAULT_RIFLE)) / weaponry.getMaxAmmo(UT2004ItemType.ASSAULT_RIFLE_AMMO));
+        groupPriority.put(UT2004ItemType.ADRENALINE_PACK, 0.0);
+    
     }
 }
