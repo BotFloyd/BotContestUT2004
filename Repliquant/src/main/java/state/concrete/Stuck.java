7d5682d91b2b171d1b2567a483980b40c985575f
Florian Seguin <Florian Seguin>
2017-05-15 15:09:46 +0200

    Modification Etat Stuck + Maj UML

--- Repliquant/src/main/java/state/concrete/Stuck.java
+++ Repliquant/src/main/java/state/concrete/Stuck.java
@@ -1,5 +1,6 @@
 package state.concrete;
 
+import cz.cuni.amis.pogamut.ut2004.agent.navigation.IUT2004Navigation;
 import main.Repliquant;
 import state.Behavior;
 
@@ -9,11 +10,17 @@ import state.Behavior;
  */
 public class Stuck extends Behavior{
 
-    
+    IUT2004Navigation navigation;
     
     @Override
     public void performs(Repliquant unBot) {
+        initVars(unBot);
         unBot.getLog().info("Entering Stuck Mode");
+        navigation.navigate(unBot.getItems().getRandomItem());
+    }
+    
+    private void initVars(Repliquant unBot){
+        navigation = unBot.getNavToUse();
     }
     
 }
